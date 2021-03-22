25af04ece540c97c77bfa37c5ca0c299b4f24a83
Fredrik Arvidsson <fredrik@fkan.se>
2021-03-22 22:55:05 +0100

    add logging to fetch artifact script and fix some minor issues

--- fetch_artifact.js
+++ fetch_artifact.js
@@ -1,12 +1,18 @@
-import { get } from 'axios';
+import axios from 'axios';
 import fs from 'fs';
 
 const artifact_name = 'main.js';
+console.log(`Artifact name: ${artifact_name}`);
 const action_repository = process.env.GITHUB_ACTION_REPOSITORY;
+console.log(`Action repository: ${action_repository}`);
 const action_ref = process.env.GITHUB_ACTION_REF;
+console.log(`Action ref: ${action_ref}`);
 const action_path = process.env.GITHUB_ACTION_PATH;
+console.log(`Action path: ${action_path}`);
 const server_url = process.env.GITHUB_SERVER_URL;
+console.log(`Server url: ${server_url}`);
 const api_url = process.env.GITHUB_API_URL;
+console.log(`Api url: ${api_url}`);
 
 const ensureSuccessStatusCode = (response) => {
   if (response.status !== 200) {
@@ -14,35 +20,51 @@ const ensureSuccessStatusCode = (response) => {
   }
 };
 
-const getReleaseArtifact = async (releaseTag) =>
-  get(
-    `${server_url}/${action_repository}/releases/download/${releaseTag}/${artifact_name}`
-  );
+const getReleaseArtifact = async (releaseTag) => {
+  const url = `${server_url}/${action_repository}/releases/download/${releaseTag}/${artifact_name}`;
+  console.log(`Getting release artifact from ${url}`);
+  return axios.get(url, { responseType: 'stream' });
+};
 
 const getReleaseTagFromAnnotatedTag = async (tag) => {
-  const tagResponse = await get(
-    `${api_url}/repos/${action_repository}/git/refs/tags/${tag}`
-  );
+  const url = `${api_url}/repos/${action_repository}/git/refs/tags/${tag}`;
+  console.log(`Fetching release tag from ${url}`);
+  const tagResponse = await axios.get(url);
 
   ensureSuccessStatusCode(tagResponse);
 
   if (tagResponse.data.object.type === 'tag') {
-    const refTagResponse = await get(tagResponse.object.url);
+    const commitUrl = tagResponse.data.object.url;
+    console.log(`Tag found, getting commit from ${commitUrl}`);
+    const refTagResponse = await axios.get(commitUrl);
 
-    return refTagResponse.data.message.trim();
+    var releaseTag = refTagResponse.data.message.trim();
+    console.log(`Tag ${tag} is pointing at release ${releaseTag}`);
+    return releaseTag;
   }
 
   throw Error(`${tag} is not an annotated tag`);
 };
 
-let getReleaseArtifactResponse = await getReleaseArtifact(action_ref);
+const run = async () => {
+  let getReleaseArtifactResponse;
+  try {
+    getReleaseArtifactResponse = await getReleaseArtifact(action_ref);
+  } catch (error) {
+    if (error.response.status !== 404) {
+      throw error;
+    }
 
-if (getReleaseArtifactResponse.status === 404) {
-  const releaseTag = await getReleaseTagFromAnnotatedTag(action_ref);
-  getReleaseArtifactResponse = await getReleaseArtifact(releaseTag);
-}
+    const releaseTag = await getReleaseTagFromAnnotatedTag(action_ref);
+    getReleaseArtifactResponse = await getReleaseArtifact(releaseTag);
+  }
 
-ensureSuccessStatusCode(getReleaseArtifactResponse);
+  ensureSuccessStatusCode(getReleaseArtifactResponse);
 
-const file = fs.createWriteStream(`${action_path}/${artifact_name}`);
-getReleaseArtifactResponse.data.pipe(file);
+  const artifactPath = `${action_path}/lib/${artifact_name}`;
+  console.log(`Writing script to ${artifactPath}`);
+  const artifactWriter = fs.createWriteStream(artifactPath);
+  getReleaseArtifactResponse.data.pipe(artifactWriter);
+};
+
+run();
