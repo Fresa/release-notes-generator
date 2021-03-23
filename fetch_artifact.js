67b863852ec1055e402102e424f500f57991c859
Fredrik Arvidsson <fredrik@fkan.se>
2021-03-24 00:13:18 +0100

    fix: catching run exceptions

--- fetch_artifact.js
+++ fetch_artifact.js
@@ -17,72 +17,70 @@ try {
   const action_path = getEnvironmentVariable('GITHUB_ACTION_PATH');
   const server_url = getEnvironmentVariable('GITHUB_SERVER_URL');
   const api_url = getEnvironmentVariable('GITHUB_API_URL');
+
+  const ensureSuccessStatusCode = (response) => {
+    if (response.status !== 200) {
+      throw Error(
+        `${response.status} ${response.statusText}: (${response.data})`
+      );
+    }
+  };
+
+  const getReleaseArtifact = async (releaseTag) => {
+    const url = `${server_url}/${action_repository}/releases/download/${releaseTag}/${artifact_name}`;
+    core.info(`Getting release artifact from ${url}`);
+    return axios.get(url, { responseType: 'stream' });
+  };
+
+  const getReleaseTagFromAnnotatedTag = async (tag) => {
+    const url = `${api_url}/repos/${action_repository}/git/refs/tags/${tag}`;
+    console.info(`Getting release tag from ${url}`);
+    const tagResponse = await axios.get(url);
+
+    ensureSuccessStatusCode(tagResponse);
+
+    if (tagResponse.data.object.type === 'tag') {
+      const commitUrl = tagResponse.data.object.url;
+      core.info(`Tag found, getting commit from ${commitUrl}`);
+      const refTagResponse = await axios.get(commitUrl);
+
+      var releaseTag = refTagResponse.data.message.trim();
+      core.info(`Tag ${tag} is pointing at release ${releaseTag}`);
+      return releaseTag;
+    }
+
+    throw Error(`${tag} is not an annotated tag`);
+  };
+
+  (async () => {
+    try {
+      let getReleaseArtifactResponse;
+      try {
+        getReleaseArtifactResponse = await getReleaseArtifact(action_ref);
+      } catch (error) {
+        if (error.response.status !== 404) {
+          throw error;
+        }
+
+        core.info('Artifact was not found, searching for release');
+        const releaseTag = await getReleaseTagFromAnnotatedTag(action_ref);
+        getReleaseArtifactResponse = await getReleaseArtifact(releaseTag);
+      }
+
+      ensureSuccessStatusCode(getReleaseArtifactResponse);
+
+      const dir = `${action_path}/lib`;
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+      }
+      const artifactPath = `${dir}/${artifact_name}`;
+      core.debug(`Writing script to ${artifactPath}`);
+      const artifactWriter = fs.createWriteStream(artifactPath);
+      getReleaseArtifactResponse.data.pipe(artifactWriter);
+    } catch (error) {
+      core.setFailed(`Action failed with error ${error}`);
+    }
+  })();
 } catch (error) {
   core.setFailed(`Action failed with error ${error}`);
 }
-
-const ensureSuccessStatusCode = (response) => {
-  if (response.status !== 200) {
-    throw Error(
-      `${response.status} ${response.statusText}: (${response.data})`
-    );
-  }
-};
-
-const getReleaseArtifact = async (releaseTag) => {
-  const url = `${server_url}/${action_repository}/releases/download/${releaseTag}/${artifact_name}`;
-  core.info(`Getting release artifact from ${url}`);
-  return axios.get(url, { responseType: 'stream' });
-};
-
-const getReleaseTagFromAnnotatedTag = async (tag) => {
-  const url = `${api_url}/repos/${action_repository}/git/refs/tags/${tag}`;
-  console.info(`Getting release tag from ${url}`);
-  const tagResponse = await axios.get(url);
-
-  ensureSuccessStatusCode(tagResponse);
-
-  if (tagResponse.data.object.type === 'tag') {
-    const commitUrl = tagResponse.data.object.url;
-    core.info(`Tag found, getting commit from ${commitUrl}`);
-    const refTagResponse = await axios.get(commitUrl);
-
-    var releaseTag = refTagResponse.data.message.trim();
-    core.info(`Tag ${tag} is pointing at release ${releaseTag}`);
-    return releaseTag;
-  }
-
-  throw Error(`${tag} is not an annotated tag`);
-};
-
-const run = async () => {
-  try {
-    let getReleaseArtifactResponse;
-    try {
-      getReleaseArtifactResponse = await getReleaseArtifact(action_ref);
-    } catch (error) {
-      if (error.response.status !== 404) {
-        throw error;
-      }
-
-      core.info('Artifact was not found, searching for release');
-      const releaseTag = await getReleaseTagFromAnnotatedTag(action_ref);
-      getReleaseArtifactResponse = await getReleaseArtifact(releaseTag);
-    }
-
-    ensureSuccessStatusCode(getReleaseArtifactResponse);
-
-    const dir = `${action_path}/lib`;
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir, { recursive: true });
-    }
-    const artifactPath = `${dir}/${artifact_name}`;
-    core.debug(`Writing script to ${artifactPath}`);
-    const artifactWriter = fs.createWriteStream(artifactPath);
-    getReleaseArtifactResponse.data.pipe(artifactWriter);
-  } catch (error) {
-    core.setFailed(`Action failed with error ${error}`);
-  }
-};
-
-run();
