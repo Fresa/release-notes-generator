571e68dc40dc9c4e1a61637b0af1f559252db33b
Fredrik Arvidsson <fresa@fresa.se>
2022-06-01 15:56:57 +0200

    Revert "test what is causing node to fail with Parameter token or opts.auth is required"
    This reverts commit e29a5326fbf037650af2047707e7ddcdd18c32de.

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,25 @@ async function run(): Promise<void> {
     const version = core.getInput('version');
     const fromRef = core.getInput('from_ref_exclusive');
     const toRef = core.getInput('to_ref_inclusive');
+    const path_to_commits = core.getInput('path_to_commits');
+
+    const commits = JSON.parse(readFileSync(path_to_commits, 'utf-8'));
+
+    const releaseNotes = await generateNotes(
+      {},
+      {
+        commits,
+        logger: { log: core.info },
+        options: {
+          repositoryUrl: `https://github.com/${process.env.GITHUB_REPOSITORY}`
+        },
+        lastRelease: { gitTag: fromRef },
+        nextRelease: { gitTag: toRef, version: version }
+      }
+    );
+
+    core.info(`Release notes: ${releaseNotes}`);
+    core.setOutput('release_notes', releaseNotes);
   } catch (error: any) {
     core.setFailed(`Action failed with error ${error}`);
   }
