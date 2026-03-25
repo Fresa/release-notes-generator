8a8788d7953de7c65c7f16e1c5e76fe42a51d671
Fredrik Arvidsson <fresa@fresa.se>
2026-03-25 22:39:16 +0100

    try using platform node to handle createRequire properly

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,13 +3,8 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/main.ts'],
   format: ['esm'],
+  platform: 'node',
   minify: true,
   splitting: false,
-  noExternal: [/.*/],
-  banner: {
-    js: `
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-`
-  }
+  noExternal: [/.*/]
 });
